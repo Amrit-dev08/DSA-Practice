915bb6f6aa3fddbeb4a7deca8514dcb0e84b46a8
Anonymousz08 <ranjnadevi588@gmail.com>
2025-08-22 21:51:11 +0530

    Sum of n Numbers using Recursion

--- Recursion/Sum_of_N_Numbers_01.java
+++ Recursion/Sum_of_N_Numbers_01.java
@@ -1,7 +1,17 @@
 package Recursion;
-public class Sum_of_N_Numbers_01 {
-    public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+class Sum_of_N_Numbers_01 {
+    static int myFun(int n) {
+        // base
+        if (n == 0) {
+            return 0;
+        }
+        return n + myFun(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int sum = myFun(n);
+        System.out.println(sum);
     }
 }
