64cadef48546d9196f06aa44bf5c5df174ec759e
Anonymousz08 <ranjnadevi588@gmail.com>
2025-08-22 22:00:05 +0530

    Checking Palindrome

---
+++ Recursion/Palindrome_03.java
@@ -0,0 +1,27 @@
+package Recursion;
+import java.util.Arrays;
+
+class Palindrome_03 {
+
+    static int[] myFun(int[] arr, int l, int r) {
+        if (l > r) {
+            return arr;
+        }
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        myFun(arr, l + 1, r - 1);
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 2, 1 };
+        int org[] = arr.clone();
+        int rev[] = myFun(arr, 0, arr.length - 1);
+        if (Arrays.equals(org, rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
