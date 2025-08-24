9ac01c42f4103578ad79aed3f08192be488568c1
Amrit <amritanand8882@gmail.com>
2025-08-24 13:25:05 +0530

    Sum of digits of a Number

---
+++ Recursion/A09_SumOfDigits_Of_A_Number.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class A09_SumOfDigits_Of_A_Number {
+    public static int myFun(int num, int sum) {
+        if (num < 1) {
+            return sum;
+        }
+        return myFun(num / 10, sum + (num % 10));
+    }
+    
+    public static void main(String[] args) {
+        int n = 2536;
+        int res = myFun(n, 0);
+        System.out.println(res);
+    }
+}
